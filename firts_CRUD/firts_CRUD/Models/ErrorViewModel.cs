8900660623c5eaad9b7682aaf476f5491c826345
tnicolereyes1388 <160084075+tnicolereyes1388@users.noreply.github.com>
2024-10-05 22:58:40 -0400

    CRUD

---
+++ firts_CRUD/firts_CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace firts_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ firts_CRUD/firts_CRUD/Models/FirstCrudContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace firts_CRUD.Models;
+
+public partial class FirstCrudContext : DbContext
+{
+    public FirstCrudContext()
+    {
+    }
+
+    public FirstCrudContext(DbContextOptions<FirstCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+            //  => optionsBuilder.UseSqlServer("server=MSI\\SQLEXPRESS; database=first_CRUD; integrated security=true; TrustServerCertificate=Yes; TimeOut=150");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__usuarios__3213E83F1233E945");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("clave");
+            entity.Property(e => e.Fecha).HasColumnName("fecha");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
